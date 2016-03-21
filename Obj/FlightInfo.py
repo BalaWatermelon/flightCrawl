cc5d419cc1c78939d1ac712586dfc461ec5300d1
Jackson <jackson1994y+git@gmail.com>
2016-03-21 17:12:22 +0800

    Change Date Search and some coding style adjust.

---
+++ Obj/FlightInfo.py
@@ -0,0 +1,11 @@
+class FlightInfo:
+    Company=""
+    DepartureTime=""
+    ArrivalTime=""
+    Price=""
+
+    def __init__(self,Company,DepartureTime,ArrivalTime,Price):
+        self.Company=Company
+        self.DepartureTime=DepartureTime
+        self.ArrivalTime=ArrivalTime
+        self.Price=Price
--- logic/peachCrawl.py
+++ logic/peachCrawl.py
@@ -4,12 +4,12 @@ import time
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
-dates = ['2016-01-17','2016-01-18','2016-01-19']
+dates = ['2016-03-24','2016-03-25','2016-03-26']
 driver = webdriver.Firefox()
 print "Peach"
 for day in dates:
     driver.get('https://book.flypeach.com/default.aspx?ao=B2CZHTW&ori=TPE&des=KIX&dep='+day+'&adt=2&chd=0&inf=0&langculture=zh-TW&bLFF=false')
-    time.sleep(4)
+    time.sleep(10)
     data = BeautifulSoup(driver.page_source,"html.parser")
     print day
     for ele in data.find_all("tr",class_=re.compile("FlightInformation")):
@@ -17,7 +17,7 @@ for day in dates:
         print ele.select(".Deptflight")[0].text.replace('\t',"").replace('\n',"").replace(' ',"")
         print ele.select(".Arrivflight")[0].text.replace('\t',"").replace('\n',"").replace(' ',"")
         print ele.select("label")[0].text.replace('\t',"").replace('\n',"").replace(' ',"")
-        print  ele.select("label")[1].text.replace('\t',"").replace('\n',"").replace(' ',"")
+        print ele.select("label")[1].text.replace('\t',"").replace('\n',"").replace(' ',"")
         print "=================="
 
 driver.close()
