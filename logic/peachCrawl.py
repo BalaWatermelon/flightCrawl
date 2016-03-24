d29b4bc3563fb540b059e91a56d9c5ceb795e61e
Jackson <jackson1994y+git@gmail.com>
2016-03-24 22:41:44 +0800

    remove debugging data, turn into pure crawler logic.

--- logic/peachCrawl.py
+++ logic/peachCrawl.py
@@ -1,23 +1,26 @@
 import requests
 import re
 import time
+import FlightInfo
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
-dates = ['2016-03-24','2016-03-25','2016-03-26']
-driver = webdriver.Firefox()
-print "Peach"
-for day in dates:
-    driver.get('https://book.flypeach.com/default.aspx?ao=B2CZHTW&ori=TPE&des=KIX&dep='+day+'&adt=2&chd=0&inf=0&langculture=zh-TW&bLFF=false')
-    time.sleep(10)
-    data = BeautifulSoup(driver.page_source,"html.parser")
-    print day
-    for ele in data.find_all("tr",class_=re.compile("FlightInformation")):
-        print ele.select("a")[0].text
-        print ele.select(".Deptflight")[0].text.replace('\t',"").replace('\n',"").replace(' ',"")
-        print ele.select(".Arrivflight")[0].text.replace('\t',"").replace('\n',"").replace(' ',"")
-        print ele.select("label")[0].text.replace('\t',"").replace('\n',"").replace(' ',"")
-        print ele.select("label")[1].text.replace('\t',"").replace('\n',"").replace(' ',"")
-        print "=================="
-
-driver.close()
+class peachCrawler:
+    def __init__():
+        dates = ['2016-03-24','2016-03-25','2016-03-26']
+    driver = webdriver.Firefox()
+    print "Peach"
+    for day in dates:
+        driver.get('https://book.flypeach.com/default.aspx?ao=B2CZHTW&ori=TPE&des=KIX&dep='+day+'&adt=2&chd=0&inf=0&langculture=zh-TW&bLFF=false')
+        time.sleep(10)
+        data = BeautifulSoup(driver.page_source,"html.parser")
+        print day
+        for ele in data.find_all("tr",class_=re.compile("FlightInformation")):
+            print ele.select("a")[0].text
+            print ele.select(".Deptflight")[0].text.replace('\t',"").replace('\n',"").replace(' ',"")
+            print ele.select(".Arrivflight")[0].text.replace('\t',"").replace('\n',"").replace(' ',"")
+            print ele.select("label")[0].text.replace('\t',"").replace('\n',"").replace(' ',"")
+            print ele.select("label")[1].text.replace('\t',"").replace('\n',"").replace(' ',"")
+            print "=================="
+            info = FlightInfo('Peach',)
+    driver.close()
